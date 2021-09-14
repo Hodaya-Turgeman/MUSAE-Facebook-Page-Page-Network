1dd9176dcb1719bc39e9259e2bfc567df2be69ea
Hodaya Siman Tov <hodaysi123@gmail.com>
2021-09-14 23:54:45 +0300

    1

--- T5.py
+++ T5.py
@@ -9,68 +9,95 @@ import collections
 from matplotlib import pylab
 from operator import itemgetter
 
-df = pd.read_csv("musae_facebook_edges.csv")                            #load database from csv file
+# df = pd.read_csv("musae_facebook_edges.csv")                            #load database from csv file
 
-g = nx.from_pandas_edgelist(df, source="id_1", target="id_2")      #graph information
-musae_facebook_target = pd.read_csv('musae_facebook_target.csv', usecols = ['id','page_type'] )
+# g = nx.from_pandas_edgelist(df, source="id_1", target="id_2")      #graph information
+# musae_facebook_target = pd.read_csv('musae_facebook_target.csv', usecols = ['id','page_type'] )
 
-df = pd.DataFrame(musae_facebook_target)
-politician=df.loc[(df['page_type'] =='politician') ]
-gP=g.subgraph(list(politician['id'].values))
-tvshows=df.loc[(df['page_type'] =='tvshow') ]
+# df = pd.DataFrame(musae_facebook_target)
 
-gP_TV = g.subgraph(list(politician['id'].values)+list(tvshows['id'].values)) 
-pageType = {}
+# politician=df.loc[(df['page_type'] =='company') ]
+
+# gP=g.subgraph(list(politician['id'].values))
+# tvshows=df.loc[(df['page_type'] =='government') ]
+# gP_TV = g.subgraph(list(politician['id'].values)+list(tvshows['id'].values))
+
+# gtvshows=g.subgraph(list(tvshows['id'].values))
+# company=df.loc[(df['page_type'] =='company') ]
+# gTV_company=g.subgraph(list(tvshows['id'].values)+list(company['id'].values)) 
+# government=df.loc[(df['page_type'] =='government') ]
+# gP_government = g.subgraph(list(politician['id'].values)+list(government['id'].values)) 
+
+# pageType = {}
+# color_map = []
 # t = []
-# with open('musae_facebook_target.csv', newline='') as csvfile:
-#     reader =csv.DictReader(csvfile)
-#     for row in reader:
-#         target[row['node']] = int(row['page_type'])
-#         # x=row['node']
-#         # y=int(row['tribe'])
-#         t.append(int(row['page_type']))
-# nx.set_node_attributes(gP_TV, target, "tribe")
-color_map = []
-t = []
-t1 = []
-t2 = []
-for node in gP_TV.nodes:
-    if(node in gP.nodes ):
-        color_map.append('yellow') 
-        t1.append(node)
-        pageType[node]=1
-    else:
-        color_map.append('blue')
-        t2.append(node) 
-        pageType[node]=2
-nx.set_node_attributes(gP_TV, pageType, "page_type")
-t = [t1, t2]
-print(nx.algorithms.community.modularity(gP_TV, t,weight='None'))
-print(nx.numeric_assortativity_coefficient(gP_TV, "page_type"))
+# t1 = []
+# t2 = []
+# for node in gP_TV.nodes:
+#     if(node in gP.nodes ):
+#         color_map.append('red') 
+#         t1.append(node)
+#         pageType[node]=1
+#     else:
+#         color_map.append('green')
+#         t2.append(node) 
+#         pageType[node]=2
+# nx.set_node_attributes(gP_TV, pageType, "page_type")
 
+# for node in gP_government.nodes:
+#     if(node in gP.nodes ):
+#         color_map.append('yellow') 
+#         t1.append(node)
+#         pageType[node]=1
+#     else:
+#         color_map.append('green')
+#         t2.append(node) 
+#         pageType[node]=2
+
+# for node in gTV_company.nodes:
+#     if(node in gtvshows.nodes ):
+#         color_map.append('blue') 
+#         t1.append(node)
+#         pageType[node]=1
+#     else:
+#         color_map.append('red')
+#         t2.append(node) 
+#         pageType[node]=2
+
+# nx.set_node_attributes(gP_TV, pageType, "page_type")
+# # nx.set_node_attributes(gTV_company, pageType, "page_type")
+# t = [t1, t2]
+# print(nx.algorithms.community.modularity(gP_TV ,t,weight='None'))
+# print(nx.numeric_assortativity_coefficient(gP_TV, "page_type"))
+# print(nx.info(gP_TV))
+# # amount of connected component
+# cs = print( 'The number Connected Components in original graph: {0}'.format(nx.number_connected_components(gP_TV)))
+# s = sorted(nx.connected_components(gP_TV), key=len, reverse=True)
+# erGiant = gP_TV.subgraph(s[0])
+# print(nx.info(erGiant))
 # nx.draw(gP_TV, node_color=color_map, with_labels=False, font_size=8)
 # plt.show()
+H = nx.read_gml("adjnoun.gml")
 
+print(H.nodes(data=True))
+# nx.draw(H, with_labels=False, font_size=8)
+
+print(nx.numeric_assortativity_coefficient(H, "value"))
 #סכום דרגות 
-sum=[0,0]
-degrees = list(gP_TV.degree())
-degree_sequenceC = sorted([f for m, f in gP_TV.degree()], reverse=True)  # degree sequence of configuration model
-maxDeg = degree_sequenceC[0]
-print(maxDeg)
 
-l=list(gP_TV.nodes.data("page_type"))
-print(degrees)
+# l=list(gP_TV.nodes.data("page_type"))
+# print(degrees)
 # print(l)
-for i in range(len(l)):
-    sum[l[i][1]-1]+=degrees[i][1]
-# print(sum)
-fig, ax = plt.subplots()
-deg=[1,2]
-colors=["yellow","blue"]
+# for i in range(len(l)):
+#     sum[l[i][1]-1]+=degrees[i][1]
+# # print(sum)
+# fig, ax = plt.subplots()
+# deg=[1,2]
+# colors=["yellow","blue"]
 
-plt.bar(deg, sum, width=0.50, color=colors,alpha=0.7)
-plt.legend(['politician','tvshow'])
-plt.title("Sum of Degrees")
-plt.ylabel("sum")
-# plt.xlabel("degree")
-plt.show()
+# plt.bar(deg, sum, width=0.50, color=colors,alpha=0.7)
+# plt.legend(['politician','tvshow'])
+# plt.title("Sum of Degrees")
+# plt.ylabel("sum")
+# # plt.xlabel("degree")
+# plt.show()
--- code.py
+++ code.py
@@ -14,25 +14,39 @@ df = pd.read_csv("musae_facebook_edges.csv")                            #load da
 g = nx.from_pandas_edgelist(df, source="id_1", target="id_2")      #graph information
 musae_facebook_target = pd.read_csv('musae_facebook_target.csv', usecols = ['id','page_type'] )
 
-# df = pd.DataFrame(musae_facebook_target)
-# politician=df.loc[(df['page_type'] =='politician') ]
-                                         #Adding adam to the list
+df = pd.DataFrame(musae_facebook_target)
+politician=df.loc[(df['page_type'] =='politician') ]
+                                        #  Adding adam to the list
 
-# nP = g.subgraph(politician['id'].values)  
-# tvshows=df.loc[(df['page_type'] =='tvshow') ]
-# nT = g.subgraph(list(politician['id'].values)+list(tvshows['id'].values))  
-# nTv=g.subgraph(tvshows['id'].values)
-# dg= nx.Graph()
-# dg.add_nodes_from(nT.nodes)
-# dg.add_edges_from(nT.edges-nP.edges-nTv.edges)
-
-# color_map = []
-# for node in dg.nodes:
-#     if(node in nTv.nodes ):
-#         color_map.append('pink') 
-#     else:
-#         color_map.append('blue') 
-
+nP = g.subgraph(politician['id'].values)  
+tvshows=df.loc[(df['page_type'] =='tvshow') ]
+nPTv = g.subgraph(list(politician['id'].values)+list(tvshows['id'].values))  
+nTv=g.subgraph(tvshows['id'].values)
+g= nx.Graph()
+g.add_nodes_from(nPTv.nodes)
+g.add_edges_from(nPTv.edges-nP.edges-nTv.edges)
+pageType = {}
+color_map = []
+t = []
+t1 = []
+t2 = []
+for node in g.nodes:
+    if(node in nP.nodes ):
+        color_map.append('yellow') 
+        t1.append(node)
+        pageType[node]=1
+    else:
+        color_map.append('green') 
+        t2.append(node)
+        pageType[node]=2
+nx.set_node_attributes(g, pageType, "page_type")
+t = [t1, t2]
+print(nx.algorithms.community.modularity(g ,t,weight='None'))
+print(nx.numeric_assortativity_coefficient(g, "page_type"))
+print(nx.info(g))
+cs = print( 'The number Connected Components in original graph: {0}'.format(nx.number_connected_components(g)))
+# nx.draw(g, node_color=color_map, with_labels=False)
+# plt.show() 
 # degrees = [dg.degree(n) for n in dg.nodes()]  #A list with all the graph's degrees
 # num0 = degrees.count(0)
 # num1 = degrees.count(1)
@@ -69,14 +83,14 @@ musae_facebook_target = pd.read_csv('musae_facebook_target.csv', usecols = ['id'
 # avgb = sum(bit.values()) / float(len(bit))
 # print('betweeness average', avgb)
 
-deg = nx.degree_centrality(g)
-resd = dict(sorted(deg.items(), key = itemgetter(1), reverse = True)[:5])
-print("The top 5 value pairs are  " + str(resd))
+# deg = nx.degree_centrality(g)
+# resd = dict(sorted(deg.items(), key = itemgetter(1), reverse = True)[:5])
+# print("The top 5 value pairs are  " + str(resd))
 
-degval = deg.values()
-min_value = min(degval)
-min_key = min(deg, key=deg.get)
-print(min_key , min_value)
+# degval = deg.values()
+# min_value = min(degval)
+# min_key = min(deg, key=deg.get)
+# print(min_key , min_value)
 # avgd = sum(deg.values()) / float(len(deg))
 # print('degree average', avgd)
 # print(nx.diameter(g))
